a7b2f9b070fa67a3514c7d07344205148c6c6ab9
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-18 18:11:57 +0300

    Completed assignment

--- server/models.py
+++ server/models.py
@@ -39,38 +39,38 @@ class Post(db.Model):
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
     category = db.Column(db.String)
-    summary = db.Column(db.String)
+    summary = db.Column(db.String(250))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     @validates("title")
-    def validate_title(self, key, value):
+    def validate_title(self, key, title):
         required_phrases = ["Won't Believe", "Secret", r"Top \d+", "Guess"]
-        if not any(phrase in value for phrase in required_phrases):
+        if not any(phrase in title for phrase in required_phrases):
             raise ValueError("Title should be clickbait-y and contain at least one of: 'Won't Believe', 'Secret', 'Top [number]', 'Guess'")
         
-        return value
+        return title
 
     @validates("content")
-    def validates_content(self, key, value):
+    def validates_content(self, key, content):
 
         #Ensure the post content is atleast 250 characters long
-        if len(value)< 250:
+        if len(content)< 250:
             raise ValueError("Post content must be atleast 250 characters long")
-        return value
+        return content
     
     @validates("summary")
-    def validate_summary(self, key, value):
-        if len(value) >= 250:
+    def validate_summary(self, key, summary):
+        if len(summary) >= 250:
             raise ValueError("Summary too long. More than 250 chars.")
-        return value
+        return summary
     
     @validates("category")
-    def validate_category(self, key, value):
-        if value not in ["Fiction", "Non-Fiction"]:
+    def validate_category(self, key, category):
+        if category !="Fiction" and category!= "Non-Fiction":
             raise ValueError("Post category must be eitherFiction or Non-Fiction")
         
-        return value
+        return category
 
 
     def __repr__(self):
